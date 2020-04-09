851c7afa9bff447ac3dc7ccf69a3db143060cd52
envis10n <envis10n@protonmail.com>
2020-04-09 12:03:40 -0500

    Update docs

--- docs/libtelnet-rs/events/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["TelnetEvent","Telnet event types."]],"struct":[["DataEvent","An event representing general data."],["NegotiationEvent","An event representing a general IAC negotiation sequence."],["SubnegotiationEvent","An event representing a subnegotiation IAC sequence."]]});
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["libtelnet-rs"] = {"name":"","files":["bytes.rs","compatibility.rs","events.rs","lib.rs","telnet.rs"]};
+sourcesIndex["libtelnet-rs"] = {"name":"","files":["bytes.rs","compatibility.rs","lib.rs","telnet.rs"]};
 createSourceSidebar();
