5eb56390a45fcb8c32dc8580f9b0a1226a0ac140
Linus Probert <linus@moth.localdomain>
2020-04-25 14:32:54 +0200

    Adds asserts to parse tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,27 +1,78 @@
 use super::*;
 
+
 /// Test the parser and its general functionality.
 
-fn handle_events(event_list: Vec<events::TelnetEvents>) {
-  for event in event_list {
-    match event {
-      events::TelnetEvents::IAC(ev) => {
-        println!("IAC: {}", ev.command);
-      }
-      events::TelnetEvents::Negotiation(ev) => {
-        println!("Negotiation: {} {}", ev.command, ev.option);
-      }
-      events::TelnetEvents::Subnegotiation(ev) => {
-        println!("Subnegotiation: {} {:?}", ev.option, ev.buffer);
-      }
-      events::TelnetEvents::DataReceive(buffer) => {
-        println!("Receive: {:?}", buffer);
-      }
-      events::TelnetEvents::DataSend(buffer) => {
-        println!("Send: {:?}", buffer);
-      }
+#[derive(PartialEq, Debug)]
+enum Event {
+    IAC,
+    NEGOTIATION,
+    SUBNEGOTIATION,
+    RECV,
+    SEND
+}
+
+macro_rules! events {
+    ( $( $x:expr ),* ) => {
+        {
+            #[allow(unused_mut)]
+            let mut temp_ce = CapturedEvents::default();
+            $(
+                temp_ce.push($x);
+            )*
+            temp_ce
+        }
+    };
+}
+
+#[derive(Default, Debug)]
+struct CapturedEvents {
+    events: Vec<Event>,
+}
+
+impl CapturedEvents {
+    fn push(&mut self, event: Event) {
+        self.events.push(event);
     }
-  }
+}
+
+impl PartialEq for CapturedEvents {
+    fn eq(&self, other: &Self) -> bool {
+        if self.events.len() == other.events.len() {
+            self.events.iter().zip(other.events.iter()).all(|(val1, val2)| { val1 == val2 })
+        } else {
+            false
+        }
+    }
+}
+
+fn handle_events(event_list: Vec<events::TelnetEvents>) -> CapturedEvents {
+    let mut events = CapturedEvents::default();
+    for event in event_list {
+        match event {
+            events::TelnetEvents::IAC(ev) => {
+                println!("IAC: {}", ev.command);
+                events.push(Event::IAC);
+            }
+            events::TelnetEvents::Negotiation(ev) => {
+                println!("Negotiation: {} {}", ev.command, ev.option);
+                events.push(Event::NEGOTIATION);
+            }
+            events::TelnetEvents::Subnegotiation(ev) => {
+                println!("Subnegotiation: {} {:?}", ev.option, ev.buffer);
+                events.push(Event::SUBNEGOTIATION);
+            }
+            events::TelnetEvents::DataReceive(buffer) => {
+                println!("Receive: {:?}", buffer);
+                events.push(Event::RECV);
+            }
+            events::TelnetEvents::DataSend(buffer) => {
+                println!("Send: {:?}", buffer);
+                events.push(Event::SEND);
+            }
+        };
+    }
+    events
 }
 
 #[test]
@@ -29,13 +80,13 @@ fn test_parser() {
   let mut instance: Parser = Parser::new();
   instance.options.support_local(201);
   if let Some(ev) = instance._will(201) {
-    handle_events(vec![ev]);
+    assert_eq!(handle_events(vec![ev]), events![Event::SEND]);
   }
-  handle_events(instance.receive(&[b"Hello, rust!", &[255, 249][..]].concat()));
-  handle_events(instance.receive(&[255, 253, 201]));
-  handle_events(
+  assert_eq!(handle_events(instance.receive(&[b"Hello, rust!", &[255, 249][..]].concat())), events![Event::RECV, Event::IAC]);
+  assert_eq!(handle_events(instance.receive(&[255, 253, 201])), events![]);
+  assert_eq!(handle_events(
     instance.receive(&events::TelnetSubnegotiation::new(201, b"Core.Hello {}").into_bytes()),
-  );
+  ), events![Event::SUBNEGOTIATION]);
 }
 
 #[test]
