7aaa9862a280ed82a8079bb5a05befe23f917c67
Linus Probert <linus.probert@gmail.com>
2020-10-29 18:44:09 +0100

    Adds Clone trait to CompatibilityTable
    When instancing a new parser and want to keep support from an existing
    one it's handy to be able to clone the compatibility table.

--- src/compatibility.rs
+++ src/compatibility.rs
@@ -49,6 +49,7 @@ impl CompatibilityEntry {
 }
 
 /// A table of options that are supported locally or remotely, and their current state.
+#[derive(Clone)]
 pub struct CompatibilityTable {
   options: [u8; 256],
 }
